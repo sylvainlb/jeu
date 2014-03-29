cf7fd9430ce898f775dda299551c57e3cf90f09b
Vincent FRANCOIS <vincent@oenotopia.fr>
2014-03-29 22:28:57 +0100

    Update : game loaded on home only

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,16 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: Vincent
+ * Date: 29/03/14
+ * Time: 22:25
+ * To change this template use File | Settings | File Templates.
+ */
+//= require underscore
+//= require backbone
+//= require jeu
+//= require_tree ../templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
+//= require_tree .
