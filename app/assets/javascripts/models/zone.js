71771a3fa4dc2866e666c28f70cd58dd2658fde4
jeu <jeu@jeu-VirtualBox.(none)>
2014-03-29 20:22:07 +0100

    backbone base

--- app/assets/javascripts/models/zone.js
+++ app/assets/javascripts/models/zone.js
@@ -1,3 +1,13 @@
 Jeu.Models.Zone = Backbone.Model.extend({
-
+    defaults: {
+        x: 0,
+        y: 0,
+        type: ""
+    },
+    initialize: function(){
+        this.set({
+            x: Math.floor(Math.random()*100),
+            y: Math.floor(Math.random()*100)
+        });
+    },
 });
