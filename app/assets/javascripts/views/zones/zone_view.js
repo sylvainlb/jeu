117c83956e8e6d767dae3656a62ad9b3e0c0acfd
yukulele <cpartiot@gmail.com>
2014-03-30 16:43:01 +0200

    upgrade: styles

--- app/assets/javascripts/views/zones/zone_view.js
+++ app/assets/javascripts/views/zones/zone_view.js
@@ -3,12 +3,13 @@ Jeu.Views.ZoneView = Backbone.View.extend({
     icon: JST['zones/icon'],
     menu: JST['zones/menu'],
   },
-  
   selectZone: function() {
     $("#panel").html(this.templates['menu'](this.model.toJSON()));
+    $('.zone.active').removeClass('active');
+    this.$el.find('.zone').addClass('active');
   },
   render: function() {
     this.el.id = "zone_"+this.model.get("id");
-    this.$el.append($(this.templates['icon'](this.model.toJSON())));
+    this.$el.html(this.templates['icon'](this.model.toJSON()));
   }
 });
