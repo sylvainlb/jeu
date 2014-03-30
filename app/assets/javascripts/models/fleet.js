8bffe25a862ae66dc0a03638389e3d972d7edb0e
Sylvain Le Bon <lebon.sylvain@gmail.com>
2014-03-30 19:13:14 +0200

    bugfix: show fleet details

--- app/assets/javascripts/models/fleet.js
+++ app/assets/javascripts/models/fleet.js
@@ -1,3 +1,8 @@
 Jeu.Models.Fleet = Backbone.Model.extend({
-  url: "/fleets"
+  url: "/fleets",
+  toJSON: function() {
+    var attr = Backbone.Model.prototype.toJSON.call(this);
+    attr.transportRoutes = Jeu.region.get("transportRoutes").where({fleet_id:this.id});
+    return attr;
+  }
 });
--- app/assets/javascripts/models/zone.js
+++ app/assets/javascripts/models/zone.js
@@ -4,8 +4,6 @@ Jeu.Models.Zone = Backbone.Model.extend({
       coordY: 0,
       type: ""
   },
-  initialize: function(){
-  },
   toJSON: function() {
     var attr = Backbone.Model.prototype.toJSON.call(this);
     attr.fleets = Jeu.region.get("fleets").where({zone_id:this.id});
