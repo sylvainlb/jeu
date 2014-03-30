84422703cd2c7610b4aff5e2abc63a8c7f1854b6
Vincent FRANCOIS <vincent@oenotopia.fr>
2014-03-30 01:32:34 +0100

    Feature : distance between zones validation

--- app/models/zone.rb
+++ app/models/zone.rb
@@ -7,4 +7,19 @@ class Zone < ActiveRecord::Base
   attr_accessible :building_type_id, :region_id
 
   validates :building_type_id, :coordX, :coordY, :region_id, :presence => :true
+
+  validates :square_radius
+
+
+  def distance_coord(x,y)
+    Math.sqrt((coordX-x)**2 + (coordY-y)**2 )
+  end
+
+  def square_radius
+    radius=40
+    tooClose=Zone.where(:coordX => (self.coordX-radius..self.coordX+radius), :coordY => (self.coordY-radius..self.coordY+radius))
+    unless tooClose.empty?
+      errors.add(:coordX,'too close to an existing zone')
+    end
+  end
 end
