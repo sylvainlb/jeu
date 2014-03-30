e4f32aa9f0b95dbd6b008bedc0f94b5a1565a63d
Mehdi <mehdi.abby@gmail.com>
2014-03-30 17:52:56 +0200

    Feature add transport_route js

--- db/migrate/20140329165333_create_stocks.rb
+++ db/migrate/20140329165333_create_stocks.rb
@@ -3,8 +3,10 @@ class CreateStocks < ActiveRecord::Migration
     create_table :stocks do |t|
       t.references :zone
       t.integer :nb_resource
+      t.references :resource_type
       t.timestamps
     end
     add_index :stocks, :zone_id
+    add_index :stocks, :resource_type_id
   end
 end
