31719d572437f1e5291ae0778d8479959e41cb91
Vincent FRANCOIS <vincent@oenotopia.fr>
2014-04-04 14:08:11 +0200

    fix #3 : user creation, with auto naming

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
   attr_accessible :name
   has_many :regions
+
+  after_create :set_default_name
+
+  def set_default_name
+    self.name="User #{self.id}"
+    self.save
+  end
 end
