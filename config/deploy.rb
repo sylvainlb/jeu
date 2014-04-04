a1c4fff051145d6e01e869629cb8a60343b6eeed
Vincent FRANCOIS <vincent@oenotopia.fr>
2014-04-04 12:00:41 +0200

    update : remove database.yml link for capistrano (using postgresql)

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,10 +28,10 @@ set :ssh_options, {
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml db/production.sqlite3}
+set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system script }
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system script}
 
 # Default value for default_env is {}
 #set :default_env, { path: "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH" }
